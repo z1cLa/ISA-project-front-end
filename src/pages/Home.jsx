97e20144416da7bc6592fdbf9e06986ebdf4beb2
z1cLa <miljanlazic@gmail.com>
2024-01-23 18:28:47 +0100

    Added basic home page design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import Navbar from "../ui/Navbar";
+import "./Home.css"
 
 function Home() {
-  return <div></div>;
+  return <div>
+    
+    <div className="App">
+      <main className="App-main">
+        <h2>Welcome to Gringos!</h2>
+        <p>Your favorite place for all things Mexican.</p>
+      </main>
+      <footer className="footer">
+        <p>© 2024 - All rights reserved</p>
+      </footer>
+    </div>
+
+  </div>;
 }
 
 export default Home;
